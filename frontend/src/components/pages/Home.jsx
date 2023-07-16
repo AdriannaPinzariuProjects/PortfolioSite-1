cd41d209e9adc9f19f2ae6077200bd9f65e35cb8
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-15 21:09:45 -0500

    Switched to 2D Planet Outlines

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Sphere } from '@react-three/drei';
+import { Flex, Box, AspectRatio, VStack } from '@chakra-ui/react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 
-function Planet({ size, position }) {
-  useFrame(({ clock }) => {
-    // Rotation logic if needed
-  });
-  
+function Planet({ size }) {
   return (
-    <mesh position={position}>
-      <Sphere args={[size, 32, 32]}>
-        <meshBasicMaterial color="white" wireframe />
-      </Sphere>
-    </mesh>
-  )
+    <AspectRatio ratio={1} w={`${size}%`} h="auto">
+      <Box borderRadius="50%" border="2px solid white" />
+    </AspectRatio>
+  );
 }
 
 function Home() {
     // Relative sizes of the planets (not to scale)
-    const planetSizes = [0.3, 0.75, 0.8, 0.4, 4, 3.3, 1.4, 1.3];
-    const planetPositions = planetSizes.map((_, i) => [2 * i, 0, 0]); // Positions of the planets (change as required)
-
+    const planetSizes = [4, 6, 6.5, 5, 8, 7, 5.5, 5];
+    
     return (
-        <div style={{ width: '100vw', height: '100vh', backgroundColor: 'black' }}>
+        <div style={{ width: '100vw', height: '100vh', backgroundColor: 'black', display: 'flex', flexDirection: 'column' }}>
             <Navbar />
-            <Canvas camera={{ position: [0, 0, 10] }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[0, 10, 5]} />
+            <Flex justify="space-between" align="center" flex="1" pt="5%" pb="5%">
                 {planetSizes.map((size, i) => (
-                  <Planet key={i} size={size} position={planetPositions[i]} />
+                  <Planet key={i} size={size} />
                 ))}
-            </Canvas>
+            </Flex>
             <Footer />
         </div>
     );
