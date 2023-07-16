687d8867fb79ba0200d274ce09ae835d675a2555
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-15 19:48:12 -0500

    HeaderSpacing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Flex, Heading, Button, Spacer } from '@chakra-ui/react';
+import { Flex, Button, Spacer, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
 import  './Component.css'
+
 const MotionButton = motion(Button);
 
 const buttonVariants = {
@@ -12,70 +13,76 @@ const buttonVariants = {
 
 function Navbar() {
   return (
-    <Flex style={{ fontFamily: 'Josefin Sans' }} mt="3" bg="transparent" color="white" boxShadow="md" position="sticky" top={0} zIndex={3}>
-      <Heading size="md" ml="2">My Portfolio</Heading>
+    <Flex alignItems="center" pt='1.5%' bg="transparent" color="white" boxShadow="md" position="sticky" top={0} zIndex={3}>
+
+      <Text fontSize=".7rem" ml="2%" fontWeight="bold">MyPortfolio</Text>
       <Spacer />
       <Link to="/">
         <MotionButton
-          mr="2"
+          mr="4"
           whileHover="hover"
           whileTap="tap"
           variants={buttonVariants}
           colorScheme="black"
           size="lg"
           color="white"
+          style={{ fontFamily: 'Josefin Sans', fontSize: '65%', letterSpacing: '0.15em' }}
         >
           HOME
         </MotionButton>
       </Link>
       <Link to="/about">
         <MotionButton
-          mr="2"
+          mr="4"
           whileHover="hover"
           whileTap="tap"
           variants={buttonVariants}
           colorScheme="black"
           size="lg"
           color="white"
+          style={{ fontFamily: 'Josefin Sans', fontSize: '65%', letterSpacing: '0.15em' }}
         >
           ABOUT
         </MotionButton>
       </Link>
       <Link to="/experience">
         <MotionButton
-          mr="2"
+          mr="4"
           whileHover="hover"
           whileTap="tap"
           variants={buttonVariants}
           colorScheme="black"
           size="lg"
           color="white"
+          style={{ fontFamily: 'Josefin Sans', fontSize: '65%', letterSpacing: '0.15em' }}
         >
           EXPERIENCES
         </MotionButton>
       </Link>
       <Link to="/hobby">
         <MotionButton
-          mr="2"
+          mr="4"
           whileHover="hover"
           whileTap="tap"
           variants={buttonVariants}
           colorScheme="black"
           size="lg"
           color="white"
+          style={{ fontFamily: 'Josefin Sans', fontSize: '65%', letterSpacing: '0.15em' }}
         >
           HOBBIES
         </MotionButton>
       </Link>
       <Link to="/location">
         <MotionButton
-          mr="2"
+          mr="4"
           whileHover="hover"
           whileTap="tap"
           variants={buttonVariants}
           colorScheme="black"
           size="lg"
           color="white"
+          style={{ fontFamily: 'Josefin Sans', fontSize: '65%', letterSpacing: '0.15em' }}
         >
           LOCATION
         </MotionButton>
