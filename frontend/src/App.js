b9ffcaa0458babe738ede3d8f22da6be06392d89
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-17 03:33:18 -0500

    New Acceleration Motion & Fixed Merge Navigation System

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import { Global, css } from "@emotion/react";
 import 'odometer/themes/odometer-theme-default.css';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
@@ -19,53 +19,19 @@ function App() {
   return (
     <ChakraProvider>
       <Router>
-        <Global
-          styles={css`
-            * {
-              margin: 0;
-              padding: 0;
-              box-sizing: border-box;
-            }
-            body {
-              overflow-x: hidden;
-            }
-            .fade-enter {
-              opacity: 0;
-            }
-            .fade-enter-active {
-              opacity: 1;
-              transition: opacity 1000ms;
-            }
-            .fade-exit {
-              opacity: 1;
-            }
-            .fade-exit-active {
-              opacity: 0;
-              transition: opacity 1000ms;
-            }
-          `}
-        />
-        <Route render={({location}) => (
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={1000} // Transition time in ms
-              classNames="fade"
-            >
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutMe />} /> 
-                <Route path="/experience" element={<Experience />} />
-                <Route path="/hobby" element={<Hobby />} />
-                <Route path="/location" element={<Location />} />
-                <Route path="/:planetName/info" element={<PlanetInfo />} /> 
-              </Routes>
-            </CSSTransition>
-          </TransitionGroup>
-        )} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutMe />} /> 
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/hobby" element={<Hobby />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/:planetName/info" element={<PlanetInfo />} /> 
+        </Routes>
       </Router>
     </ChakraProvider>
   );
 }
+
+           
 
 export default App;
