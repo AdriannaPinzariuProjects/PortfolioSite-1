5c7585a4ec8b35f06654baed364909f2e6ce794f
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-16 19:27:40 -0500

    taking a break

--- frontend/src/components/pages/PlanetInfo.js
+++ frontend/src/components/pages/PlanetInfo.js
@@ -7,11 +7,14 @@ import Navbar from '../Navbar';
 import Footer from '../Footer';
 import planetDetails from './PlanetDetails';
 import backgroundImage from '../../Assets/testPlanet1.jpg';
+import numberMask from '../../Assets/01.svg';
+
 
 const PlanetInfo = () => {
     const { planetName } = useParams(); 
     const planet = planetDetails.find(p => p.name === planetName);
 
+    
     const bgImage = {
         backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0,.9) 50%, rgba(0, 0, 0, 0.7) 60%, rgba(0, 0, 0, 0.3) 70%, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 0) 90%), url(${backgroundImage})`,
         backgroundSize: 'cover',
@@ -43,42 +46,70 @@ const PlanetInfo = () => {
 
     return (
         <Box style={bgImage}>
-        <Flex direction="column" h="100vh">
-        <Navbar />
-          <Flex direction="column" flex="1">
-            <Box style={fadeImageStyle}>
-            </Box>
-          </Flex>
-          <Box bg="black" flex="0.4">
-            <Flex justifyContent="space-around" alignItems="center" w="100%" color="white" h="100%">
-                <Stack spacing={1}>
-                  <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">ONE WAY LIGHT TIME TO SUN MINS</Text>
-                  <Text className="opensans-light" >
-                    <AnimatedNumber start={43.810231} value={44.909316} duration={3000} formatValue={n => n.toFixed(6)} />
-                  </Text>
-                </Stack>
-                <Stack spacing={1}>
-                  <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">DISTANCE TO SUN MILES</Text>
-                  <Text className="opensans-light">
-                    <AnimatedNumber start={5023149120} value={5023149307} duration={3000} formatValue={n => Math.floor(n).toLocaleString()} />
-                  </Text>
-                </Stack>
-                <Stack spacing={1}>
-                  <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">LENGTH OF YEAR</Text>
-                  <Flex alignItems="center">
-                  <Text className="opensans-light"  >
-                    <AnimatedNumber start={4301} value={4333} duration={3000} formatValue={n => Math.floor(n).toLocaleString()} />
-                    {" Earth days"}
-                    </Text>
-                    </Flex>
-                  
-                </Stack>
+            <Flex direction="column" h="100vh" position="relative">
+                <Navbar />
+                <Text style={{
+      fontSize: "5em",
+      fontWeight: 700,
+      background: `url(${backgroundImage})`,
+      WebkitBackgroundClip: "text",
+      WebkitTextFillColor: "white"
+    }}>
+    01
+</Text>
+
+            <Flex direction="column" flex="1">
+                <Box style={fadeImageStyle}>
+                </Box>
             </Flex>
-          </Box> 
-          <Footer flex="0.1"/>
-        </Flex>
-      </Box>
+            <Box 
+              pos="relative"
+              bg="black" 
+              flex="0.4"
+              _after={{
+                  content: '""',
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0,.9) 50%, rgba(0, 0, 0, 0.7) 60%, rgba(0, 0, 0, 0.3) 70%, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 0) 90%)`,
+                  maskImage: `url(${numberMask})`,
+                  maskSize: 'cover',
+                  maskPosition: 'center',
+                  zIndex: 1,
+                }}
+            >
+                <Flex justifyContent="space-around" alignItems="center" w="100%" color="white" h="100%" pos="relative" zIndex={2}>
+                    <Stack spacing={1}>
+                        <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">ONE WAY LIGHT TIME TO SUN MINS</Text>
+                        <Text className="opensans-light" >
+                            <AnimatedNumber start={43.810231} value={44.909316} duration={3000} formatValue={n => n.toFixed(6)} />
+                        </Text>
+                    </Stack>
+                    <Stack spacing={1}>
+                        <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">DISTANCE TO SUN MILES</Text>
+                        <Text className="opensans-light">
+                            <AnimatedNumber start={5023149120} value={5023149307} duration={3000} formatValue={n => Math.floor(n).toLocaleString()} />
+                        </Text>
+                    </Stack>
+                    <Stack spacing={1}>
+                        <Text className="opensans-extralight" fontSize=".85vw" textTransform="uppercase">LENGTH OF YEAR</Text>
+                        <Flex alignItems="center">
+                        <Text className="opensans-light"  >
+                            <AnimatedNumber start={4301} value={4333} duration={3000} formatValue={n => Math.floor(n).toLocaleString()} />
+                            {" Earth days"}
+                        </Text>
+                        </Flex>
+                    
+                    </Stack>
+                </Flex>
+            </Box> 
+            <Footer flex="0.1"/>
+          </Flex>
+        </Box>
     );
+
 };
 
 export default PlanetInfo;
