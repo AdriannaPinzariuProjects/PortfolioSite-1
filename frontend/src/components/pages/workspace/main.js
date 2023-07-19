6045854e80934353362c27afcec62634d42b8c1e
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-19 13:50:09 -0500

    Space Invaders Frontend

--- frontend/src/components/pages/workspace/main.js
+++ frontend/src/components/pages/workspace/main.js
@@ -3,13 +3,17 @@ import GameView from './gameView.js';
 import GameController from './gameController.js';
 
 const model = new GameModel();
-const view = new GameView();
+const view = new GameView(model);
 const controller = new GameController(model, view);
 
 function gameLoop() {
-  model.update();
-  view.render();
-  requestAnimationFrame(gameLoop);
+  if (!model.gameOver) {
+    model.update();
+    view.render();
+    requestAnimationFrame(gameLoop);
+  } else {
+    console.log("Game Over!");
+  }
 }
 
 gameLoop();
